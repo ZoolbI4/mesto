57797454eaf2051afcc2e31e85e19b5ac88c7e59
ZooLbI4 <Jecson_92@mail.ru>
2023-01-28 13:51:18 +0700

    Первая проверка практическая место

---
+++ vendor/scripts/index.js
@@ -0,0 +1,30 @@
+const popup = document.querySelector(".popup");
+const popupForm = popup.querySelector(".popup__container");
+const popupOpen = document.querySelector('.profile__edit-button');
+const popupClose = popup.querySelector(".popup__сlose");
+const profileName = document.querySelector (".profile__title");
+const profileProffesional = document.querySelector(".profile__subtitle");
+const nameInput = popup.querySelector ("#name");
+const proffesionalInput = popup.querySelector ("#proffesional");
+
+function openPopup() {
+    popup.classList.add('popup_open');
+    nameInput.value = profileName.textContent;
+    proffesionalInput.value = profileProffesional.textContent;
+}
+
+function closePopup() {
+    popup.classList.remove('popup_open');
+}
+
+function editFormPopup(evt) {
+evt.preventDefault();
+profileName.textContent = nameInput.value;
+profileProffesional.textContent = proffesionalInput.value;
+closePopup();
+}
+
+popupForm.addEventListener('submit', editFormPopup);
+popupOpen.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
+
